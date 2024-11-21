57bf2eb989f33e0ce479673b7663f3e7fe6ae7e1
Marie Rohler <cc231054@fhstp.ac.at>
2024-11-21 21:04:02 +0100

    Added @Dao to SpellDao.kt

--- app/src/main/java/dev/cc231054/demonstrator_2/data/db/SpellDao.kt
+++ app/src/main/java/dev/cc231054/demonstrator_2/data/db/SpellDao.kt
@@ -1,11 +1,13 @@
 package dev.cc231054.demonstrator_2.data.db
 
+import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
 import androidx.room.Update
 import kotlinx.coroutines.flow.Flow
 
+@Dao
 interface SpellDao {
     @Insert
     suspend fun addSpell(spellEntity: SpellEntity)
